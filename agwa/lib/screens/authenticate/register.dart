3342325baadfcf238fe8229c0a62ec6940589f6f
Eugocode <dmcawaling@up.edu.ph>
2023-06-15 22:01:04 +0800

    Update register.dart

--- agwa/lib/screens/authenticate/register.dart
+++ agwa/lib/screens/authenticate/register.dart
@@ -30,67 +30,132 @@ class _RegisterState extends State<Register> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0.0,
-        title: Text('Register to Agwa'),
-        actions: <Widget>[
-          TextButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('Sign In'),
-              onPressed: () {
-                widget.toggleView();
-              })
-        ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: double.infinity,
+          // padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(
-                    hintText: 'Enter a valid email'),
-                validator: (val) => val!.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
+              Expanded(child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(top: 100),
+                        height: 200,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/logo_agwa.png"),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                      Text("Register",
+                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+                      SizedBox(height: 10),
+                      Text("Register an account",
+                        style: TextStyle(
+                            fontSize: 15,
+                            color:Colors.grey[700]),)
+                    ],
+                  ),
+                  Form(
+              key: _formKey,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  children: <Widget>[
+                    // SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter a valid email',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      validator: (val) => val!.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter a valid password',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      obscureText: true,
+                      validator: (val) =>
+                      val!.length < 6 ? 'Enter a password 6+ chars long' : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+
+                    Padding(
+                      padding:EdgeInsets.symmetric(horizontal: 40),
+                      child: MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            dynamic result = await _auth.registerWithEmailAndPassword(
+                                email, password);
+                            if (result == null) {
+                              setState(() => error = 'Please supply a valid email.');
+                            }
+                          }
+                        },
+                        color: Color(0xff0095FF),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Text(
+                          "Register",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(
-                    hintText: 'Enter a valid password'),
-                obscureText: true,
-                validator: (val) =>
-                    val!.length < 6 ? 'Enter a password 6+ chars long' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 0, 113, 62)),
-                child: Text('Register',
-                    style: const TextStyle(color: Colors.white)),
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    dynamic result = await _auth.registerWithEmailAndPassword(
-                        email, password);
-                    if (result == null) {
-                      setState(() => error = 'Please supply a valid email.');
-                    }
-                  }
-                },
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              ),
-            ],
-          ),
+            ),
+                  Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Already have an account?"),
+              GestureDetector(
+                  onTap:() {
+                    // Navigate to the register page
+                    widget.toggleView();
+                  },
+                  child: Text(
+                    "Log in",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                      color: Colors.blue,
+                    ),)
+                ),
+              ],
+            ),
+              ],
+              ),),
+            ],),
         ),
-      ),
+      )
     );
   }
 }
